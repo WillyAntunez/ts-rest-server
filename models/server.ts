4aec0731c4cb0201513d0bd38822494007fa9da8
Willy Antunez <willyantunezg@gmail.com>
2023-03-22 16:58:06 -0600

    first commit

---
+++ models/server.ts
@@ -0,0 +1,55 @@
+import express, { Application } from 'express';
+import userRoutes from '../routes/usuario';
+import cors from 'cors';
+import db from '../db/connection';
+
+class Server {
+
+    private app:Application;
+    private port:string;
+    private apiPaths = {
+        usuarios: '/api/usuarios',
+    }
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || "8080";
+
+        this.dbConnection();
+        this.middlewares();
+        this.routes();
+    }
+
+    async dbConnection(){
+        try {
+            await db.authenticate();
+            console.log('Database online');
+        } catch (error) {
+            throw new Error( 'Error conectando con la base de datos' );
+        }
+    }
+
+    middlewares(){
+        // CORS
+        this.app.use(cors());
+
+        // Lectura del body
+        this.app.use( express.json() );
+
+        // Carpeta publica
+        this.app.use( express.static('public') );
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRoutes)
+    }
+
+    listen():void {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto: ' + this.port);
+        })
+    }
+
+}
+
+export default Server;
